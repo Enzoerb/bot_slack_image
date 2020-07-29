953479097f00f306b481e78ced55f89a31aa2bfd
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-07-29 09:28:46 -0300

    now it sends the date and accepts more than 1 argument at once

--- slack_bot.py
+++ slack_bot.py
@@ -1,23 +1,29 @@
-from slack_info import TOKEN
+from slack_info import TOKEN, WEBHOOOK
+from datetime import datetime
 import requests
 import json
 from sys import argv
 from os import path
 
 if len(argv) > 1:
-    image_location = argv[1]
+    paths = argv[1:]
 else:
-    image_location = '/home/semantix/Documentos/Desafios/svg_coronavirus(p6)/svg_data/corona_us.svg'
+    paths = ['/home/semantix/Documentos/Desafios/svg_coronavirus(p6)/svg_data/corona_brazil.png']
 
-filename = path.basename(image_location)
+date_data = {'text': f'{datetime.now()}'}
+requests.post(WEBHOOOK, data=json.dumps(date_data))
 
-with open(f'{image_location}', 'rb') as image:
+for image_location in paths:
 
-    param = { "filename": filename,
-              "token": TOKEN,
-              "channels": "graficos-coronavirus" }
+    filename = path.basename(image_location)
 
-    files = dict()
-    files['file'] = image
+    with open(f'{image_location}', 'rb') as image:
 
-    requests.post("https://slack.com/api/files.upload", params=param, files=files)
+        param = { "filename": filename,
+                  "token": TOKEN,
+                  "channels": "graficos-coronavirus" }
+
+        files = dict()
+        files['file'] = image
+
+        requests.post("https://slack.com/api/files.upload", params=param, files=files)
